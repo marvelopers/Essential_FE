b7aecfc90b8159de49fb5550a3dede1b7226bb67
marvelopers <marvelopers@gmail.com>
2020-07-25 14:57:27 +0900

    mouseEvent

---
+++ Project_03/main.js
@@ -0,0 +1,18 @@
+const vertivcal = document.querySelector('.vertical');
+const horizontal = document.querySelector('.horizontal');
+const target = document.querySelector('.target');
+const tag = document.querySelector('.tag');
+
+//mouse move
+document.addEventListener('mousemove', (event) => {
+    const x = event.clientX;
+    const y = event.clientY;
+
+    vertivcal.style.left = `${x}px`;
+    horizontal.style.top = `${y}px`;
+    target.style.left = `${x}px`;
+    target.style.top = `${y}px`;
+    tag.style.left = `${x}px`;
+    tag.style.top = `${y}px`;
+    tag.innerHTML = `${x}px, ${y}px`;
+})
